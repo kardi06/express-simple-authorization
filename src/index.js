d7d4473fee743c7add9157a4aca509e82c6ebd54
Kardi <kgland06@gmail.com>
2024-11-01 16:53:06 +0700

    mongo db connection

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
+const dbConnect = require("./config/dbConnect");
+
+dbConnect();
 const app = express();
+
+//middleware
+app.use(express.json());
+
+//Routes
+
+//START SERVER
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
