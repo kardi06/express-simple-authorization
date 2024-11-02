973d13e227d440f368ad80e222e7ed8250221499
Kardi <kgland06@gmail.com>
2024-11-02 16:53:40 +0700

    register and login logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const dbConnect = require("./config/dbConnect");
+const authRoutes = require("./routes/authRoutes");
 
 dbConnect();
 const app = express();
@@ -9,7 +10,7 @@ const app = express();
 app.use(express.json());
 
 //Routes
-
+app.use("/api/auth", authRoutes);
 //START SERVER
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
