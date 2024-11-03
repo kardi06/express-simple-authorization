657dbed351726a4c1d1674b49a6e6680b6f2a09f
Kardi <kgland06@gmail.com>
2024-11-03 09:42:23 +0700

    role authorization

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const verifyToken = require("../middlewares/authMiddleware");
+const authorizeRole = require("../middlewares/roleMiddleware");
 const router = express.Router();
 
 //Only admin
-router.get("/admin", verifyToken, (req, res) => {
+router.get("/admin", verifyToken, authorizeRole("admin"), (req, res) => {
     res.json({message: "Welcome Admin"})
 })
 //Both admin and manager can access router
-router.get("/manager", verifyToken, (req, res) => {
+router.get("/manager", verifyToken, authorizeRole("admin", "manager"), (req, res) => {
     res.json({message: "Welcome Manager"})
 })
 
 //all can access this router
-router.get("/user", verifyToken, (req, res) => {
+router.get("/user", verifyToken, authorizeRole("admin", "manager", "user"), (req, res) => {
     res.json({message: "Welcome User"})
 })
 
